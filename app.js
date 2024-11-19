1a63423fe498ed8203df2f6cbde4a47d6fe73984
Lerian Febriana <kanglerian@gmail.com>
2024-11-19 09:47:42 +0700

    ict

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const allowedOriginSocket = [
   'http://localhost',
   'http://127.0.0.1:5173',
   'http://localhost:5173',
+  'https://helpdesk.politekniklp3i-tasikmalaya.ac.id',
 ];
 
 const app = express();
