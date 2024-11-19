20bd3dca5b61298ec8374a37ea3bacc9a945fedf
Lerian Febriana <kanglerian@gmail.com>
2024-11-19 10:44:23 +0700

    sip'

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-const { USERNAME, PASSWORD, DATABASE, HOST } = process.env;
+const { DB_USERNAME, DB_PASSWORD, DB_NAME, DB_HOST, DB_DIALECT } = process.env;
 module.exports = {
   "development": {
-    "username": USERNAME,
-    "password": PASSWORD,
-    "database": DATABASE,
-    "host": HOST,
-    "dialect": "mysql"
+    "username": DB_USERNAME,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOST,
+    "dialect": DB_DIALECT
   },
   "test": {
     "username": "root",
